a53a8c592ecede61c3399690d1e2f4e27c3befa9
Swapnaja27 <142212367+Swapnaja27@users.noreply.github.com>
2025-09-25 10:50:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+let div = document.querySelector(".container"); // get single container
+for (let i = 0; i <= 800; i++) {
+    let square = document.createElement("div");
+    square.classList.add("square");
+    
+square.addEventListener('mouseover',()=>{
+	square.style.transition:"none";
+})
+square.addEventListener('mouseout',()=>{
+	square.style.backgroundColor="green";
+	square.style.transition="background-color 1s ease";
+})
+	div.appendChild(square);
+}
